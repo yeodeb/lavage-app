caf431546cc5f5654c37e414d542b8289b5ec922
cstech <yeoyaya1@gmail.com>
2019-10-24 13:30:26 +0000

    2 commit

--- db/dblavage.sql
+++ db/dblavage.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  ven. 18 oct. 2019 à 12:48
+-- Généré le :  jeu. 24 oct. 2019 à 13:12
 -- Version du serveur :  5.7.21
--- Version de PHP :  7.1.16
+-- Version de PHP :  7.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `db_lavage`
+-- Base de données :  `dblavage`
 --
 
 -- --------------------------------------------------------
@@ -35,27 +35,16 @@ CREATE TABLE IF NOT EXISTS `lavage_prestation` (
   `prix` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `date` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Structure de la table `migration_versions`
+-- Déchargement des données de la table `lavage_prestation`
 --
 
-DROP TABLE IF EXISTS `migration_versions`;
-CREATE TABLE IF NOT EXISTS `migration_versions` (
-  `version` varchar(14) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `executed_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
-  PRIMARY KEY (`version`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Déchargement des données de la table `migration_versions`
---
-
-INSERT INTO `migration_versions` (`version`, `executed_at`) VALUES
-('20191018092907', '2019-10-18 09:29:44');
+INSERT INTO `lavage_prestation` (`id`, `libelle`, `prix`, `date`) VALUES
+(1, 'lavage simple', '2000', '2019-10-18 00:00:00'),
+(2, 'vidange', '3400', '2019-10-19 10:06:12'),
+(3, 'lavage complet', '9000', '2019-10-19 10:10:33');
 
 -- --------------------------------------------------------
 
@@ -79,9 +68,9 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 INSERT INTO `user` (`id`, `email`, `password`, `is_active`, `roles`) VALUES
-(1, 'yeo@gmail.com', '$2y$13$clhoUvRqqtom7rw7AFiKC.5Zx0Iw/JZShwxj8NlLMI/xCfpp0Jgj6', 1, 'a:0:{}'),
-(2, 'lyne@gmail.com', '$2y$13$xhpOWHT345eS8YC25i6aLuyjWPPLcYcTvWR5BWBCVc.N.XFE42.hK', 1, 'a:1:{i:0;s:10:\"ROLE_ADMIN\";}'),
-(3, 'ali@gmail.com', '$2y$13$x2xgOIDMedNznwtMk1ITsOSCQAlUNJE02fQKcMBpptvIHOocLVfue', 1, 'a:1:{i:0;s:9:\"ROLE_USER\";}');
+(1, 'user@gmail.com', '$2y$13$Horz2zg0ECjoJ0l.3AXsa.4xtwBneYCTTIbJ0oMObyjImvStnV9O6', 1, 'a:1:{i:0;s:9:\"ROLE_USER\";}'),
+(2, 'admin@gmail.com', '$2y$13$tG41JA/Ob/fnwKLX6aZT.uj4cG2vdB7acfFBbZvFnme/N6Pyuqsy.', 1, 'a:1:{i:0;s:10:\"ROLE_ADMIN\";}'),
+(3, 'admin1@gmail.com', '$2y$13$DsDHnllngS8JDkbi/u.I1OaI7/lZqhqRb9mWMAY86qP81q6nfdvti', 1, 'a:1:{i:0;s:10:\"ROLE_ADMIN\";}');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -22,3 +22,5 @@ class UserType extends AbstractType {
         ;
     }
 }
+
+
